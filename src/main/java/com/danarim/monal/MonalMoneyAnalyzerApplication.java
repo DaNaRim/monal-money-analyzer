f07e4ac39c0a071700f5eb267f4545647e5664e2
DaNaRim <rangar.danarim@gmail.com>
2022-10-22 20:44:54 +0300

    INIT

---
+++ src/main/java/com/danarim/monal/MonalMoneyAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.danarim.monal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonalMoneyAnalyzerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MonalMoneyAnalyzerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/danarim/monal/MonalMoneyAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danarim.monal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonalMoneyAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
