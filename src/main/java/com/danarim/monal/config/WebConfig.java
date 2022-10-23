abb64d86461f9c099dce8a2df31127d58e86c06b
DaNaRim <rangar.danarim@gmail.com>
2022-10-23 15:00:32 +0300

    INIT setup frontend and db

---
+++ src/main/java/com/danarim/monal/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.danarim.monal.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("forward:/index.html");
+        registry.addViewController("/{x:[\\w\\-]+}").setViewName("forward:/index.html");
+        registry.addViewController("/{x:^(?!api$).*$}/*/{y:[\\w\\-]+}").setViewName("forward:/index.html");
+    }
+
+}
