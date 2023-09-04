a06bb507ba316c8bab27a8d6d47d36d58b8eddf2
Nazar Horshevikov <rangar.danarim@gmail.com>
2023-09-04 16:42:58 +0300

    Frontend: FIX amount display when amount in exponential format

--- src/main/frontend/src/common/utils/moneyUtils.ts
+++ src/main/frontend/src/common/utils/moneyUtils.ts
@@ -1,6 +1,6 @@
 export function addSpacesToNumber(number: number | string): string {
     // Add space between groups of three digits
-    const numberParts = number.toString().split(".");
+    const numberParts = Number(number).toFixed(8).replace(/[.]?0+$/, "").split(".");
     const integerGroups = numberParts[0].match(/(\d+?)(?=(\d{3})+(?!\d)|$)/g) ?? [];
 
     const sign = number < 0 ? "-" : "";
