9fe7fa89740929906e4e93c58f66b792dc695006
Nazar Horshevikov <rangar.danarim@gmail.com>
2023-10-08 21:26:16 +0300

    Backend: correct tests in WalletServiceImplTest

--- src/test/java/com/danarim/monal/money/service/WalletServiceImplTest.java
+++ src/test/java/com/danarim/monal/money/service/WalletServiceImplTest.java
@@ -7,7 +7,6 @@ import com.danarim.monal.money.persistence.dao.WalletDao;
 import com.danarim.monal.money.persistence.model.Currency;
 import com.danarim.monal.money.persistence.model.Wallet;
 import com.danarim.monal.money.web.dto.CreateWalletDto;
-import com.danarim.monal.user.persistence.dao.UserDao;
 import com.danarim.monal.user.persistence.model.User;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -27,7 +26,6 @@ import static org.mockito.Mockito.when;
 class WalletServiceImplTest {
 
     private final WalletDao walletDao = mock(WalletDao.class);
-    private final UserDao userDao = mock(UserDao.class);
 
     @InjectMocks
     private WalletServiceImpl walletService;
@@ -36,14 +34,14 @@ class WalletServiceImplTest {
     void createWallet_basicCurrency() {
         CreateWalletDto walletDto = new CreateWalletDto("test", 1.0112, "USD");
 
-        when(userDao.existsById(1L)).thenReturn(true);
+        when(walletDao.existsByOwnerIdAndName(1L, "test")).thenReturn(false);
         when(walletDao.save(any(Wallet.class)))
                 .thenAnswer(invocation -> invocation.getArgument(0));
 
         Wallet result = walletService.createWallet(walletDto, 1L);
 
         assertEquals(1.01, result.getBalance());
-        verify(userDao).existsById(1L);
+        verify(walletDao).existsByOwnerIdAndName(1L, "test");
         verify(walletDao).save(any(Wallet.class));
     }
 
@@ -51,43 +49,29 @@ class WalletServiceImplTest {
     void createWallet_cryptoCurrency() {
         CreateWalletDto walletDto = new CreateWalletDto("test", 1.0112, "BTC");
 
-        when(userDao.existsById(1L)).thenReturn(true);
+        when(walletDao.existsByOwnerIdAndName(1L, "test")).thenReturn(false);
         when(walletDao.save(any(Wallet.class)))
                 .thenAnswer(invocation -> invocation.getArgument(0));
 
         Wallet result = walletService.createWallet(walletDto, 1L);
 
         assertEquals(1.0112, result.getBalance());
-        verify(userDao).existsById(1L);
+
+        verify(walletDao).existsByOwnerIdAndName(1L, "test");
         verify(walletDao).save(any(Wallet.class));
-    }
-
-    @Test
-    void createWallet_userNotFound_BadRequestException() {
-        CreateWalletDto walletDto = new CreateWalletDto("test", 0.0, "USD");
-
-        when(userDao.existsById(1L)).thenReturn(false);
-
-        BadRequestException e = assertThrows(BadRequestException.class,
-                                             () -> walletService.createWallet(walletDto, 1L));
-
-        assertEquals("validation.user.notFound", e.getMessageCode());
-        verify(userDao).existsById(1L);
-        verify(walletDao, never()).save(any(Wallet.class));
     }
 
     @Test
     void createWallet_userHasWalletWithName_BadFieldException() {
         CreateWalletDto walletDto = new CreateWalletDto("test", 0.0, "USD");
 
-        when(userDao.existsById(1L)).thenReturn(true);
         when(walletDao.existsByOwnerIdAndName(1L, "test")).thenReturn(true);
 
         BadFieldException e = assertThrows(BadFieldException.class,
                                            () -> walletService.createWallet(walletDto, 1L));
 
         assertEquals("validation.wallet.name-for-user.alreadyExists", e.getMessageCode());
-        verify(userDao).existsById(1L);
+
         verify(walletDao).existsByOwnerIdAndName(1L, "test");
         verify(walletDao, never()).save(any(Wallet.class));
     }
@@ -96,13 +80,13 @@ class WalletServiceImplTest {
     void createWallet_invalidCurrency_throwException() {
         CreateWalletDto walletDto = new CreateWalletDto("test", 0.0, "invalid");
 
-        when(userDao.existsById(1L)).thenReturn(true);
+        when(walletDao.existsByOwnerIdAndName(1L, "test")).thenReturn(false);
 
         BadRequestException e = assertThrows(BadRequestException.class,
                                              () -> walletService.createWallet(walletDto, 1L));
 
         assertEquals("validation.wallet.invalid.currency", e.getMessageCode());
-        verify(userDao).existsById(1L);
+        verify(walletDao).existsByOwnerIdAndName(1L, "test");
         verify(walletDao, never()).save(any(Wallet.class));
     }
 
