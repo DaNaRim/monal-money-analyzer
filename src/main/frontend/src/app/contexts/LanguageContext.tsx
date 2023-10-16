ff08392b92e42cd1bac43f538a3049e2af6ffeb1
Nazar Horshevikov <rangar.danarim@gmail.com>
2023-10-16 19:00:02 +0300

    Improve language logic. Add initializing to interface language

--- src/main/frontend/src/app/contexts/LanguageContext.tsx
+++ src/main/frontend/src/app/contexts/LanguageContext.tsx
@@ -1,25 +1,25 @@
-import React, { useContext, useMemo, useState } from "react";
+import { createContext, type ReactNode, useContext, useEffect, useMemo, useState } from "react";
+import LocalizedStrings from "react-localization";
+import localization from "../../i18n";
 
 const COOKIE_LOCALE_KEY = "locale";
 
 interface ContextType {
-    language: string;
+    /**
+     * language - current language. If undefined, then initializing required.
+     * Initializing is done inside useEffect in {@link LanguageContextProvider}.
+     */
+    language: string | undefined;
     changeLanguage: (language: string) => void;
 }
 
-const supportedLanguages = [
-    "en",
-    "uk",
-];
+export const DEFAULT_LOCALE = "en";
 
-const defaultLocale = "en";
+const getLocaleFromCookie = (): string | undefined =>
+    document.cookie.split("; ").find(row => row.startsWith(`${COOKIE_LOCALE_KEY}=`))?.split("=")[1];
 
-const localeFromCookie = document.cookie.split("; ")
-        .find(row => row.startsWith(`${COOKIE_LOCALE_KEY}=`))?.split("=")[1]
-    ?? defaultLocale;
-
-const LanguageContext = React.createContext<ContextType>({
-    language: localeFromCookie,
+const LanguageContext = createContext<ContextType>({
+    language: getLocaleFromCookie(),
     changeLanguage: () => {
     },
 });
@@ -28,27 +28,45 @@ const updateLocaleCookie = (locale: string): void => {
     document.cookie = `${COOKIE_LOCALE_KEY}=${locale}; path=/; secure;`;
 };
 
-const initLocaleCookie = (): void => {
-    if (document.cookie.split("; ")
-        .find(row => row.startsWith(`${COOKIE_LOCALE_KEY}=`)) === undefined) {
-        updateLocaleCookie(defaultLocale);
-    }
-};
-
 export const useLanguageContext: () => ContextType = () => useContext(LanguageContext);
 
-const LanguageContextProvider = ({ children }: { children: React.ReactNode }): JSX.Element => {
-    const [language, setLanguage] = useState(localeFromCookie);
+/**
+ * Provider for {@link LanguageContext}.
+ * When initialized, it tries to get locale from cookie. If it is not set, then it uses undefined.
+ * Further initialization is done inside useEffect.
+ * When language is changed, it updates cookie.
+ *
+ * @param children - children to render.
+ */
+const LanguageContextProvider = ({ children }: { children: ReactNode }): JSX.Element => {
+    const [language, setLanguage] = useState(getLocaleFromCookie());
 
-    initLocaleCookie();
+    const localStrings = new LocalizedStrings(localization);
 
     const changeLanguage = (language: string): void => {
-        if (supportedLanguages.includes(language)) {
+        if (localStrings.getAvailableLanguages().includes(language)) {
             setLanguage(language);
             updateLocaleCookie(language);
         }
     };
     const providerValue = useMemo(() => ({ language, changeLanguage }), [language, changeLanguage]);
+
+    useEffect(() => {
+        // If the language is set (cookie), use it.
+        if (language != null) {
+            return;
+        }
+        // If the language is not set, try to set it to the interface language.
+        const interfaceLanguage = localStrings.getInterfaceLanguage();
+        for (const lang of localStrings.getAvailableLanguages()) {
+            if (interfaceLanguage.startsWith(lang)) {
+                changeLanguage(lang);
+                return;
+            }
+        }
+        // If the interface language is not supported, set the language to the default.
+        changeLanguage(DEFAULT_LOCALE);
+    }, []);
 
     return (
         <LanguageContext.Provider value={providerValue}>
--- src/main/frontend/src/app/hooks/translation.ts
+++ src/main/frontend/src/app/hooks/translation.ts
@@ -7,6 +7,17 @@ import { useLanguageContext } from "../contexts/LanguageContext";
 /**
  * Hook for getting the translation object.
  *
+ * <p>Example usage:
+ * <pre>
+ *     const t = useTranslation();
+ *     return <div>{t.hello}</div>;
+ *
+ *     // If the language is set to "en", this will render:
+ *     // <div>Hello</div>
+ *     // If the language is set to "uk", this will render:
+ *     // <div>Привіт</div>
+ *     // Don't forget to add the translations to the localization object (i18n/index.ts).
+ *
  * <p>To typify the translations, use this: (t: LocalizedStrings<Localization>)
  * from {@link Localization i18n/index.ts}
  *
@@ -16,7 +27,9 @@ function useTranslation(): LocalizedStringsType<Localization> {
     const { language } = useLanguageContext();
     const translation = new LocalizedStrings(localization);
 
-    translation.setLanguage(language);
+    if (language != null) {
+        translation.setLanguage(language);
+    }
     return translation;
 }
 
