41da2882d700ca825e84688c5abfa2e246eda5f4
Nazar Horshevikov <rangar.danarim@gmail.com>
2023-10-23 19:58:23 +0300

    Fix not found error page display when non-latin character used in url

--- src/main/java/com/danarim/monal/config/WebConfig.java
+++ src/main/java/com/danarim/monal/config/WebConfig.java
@@ -13,6 +13,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
@@ -77,9 +78,21 @@ public class WebConfig implements WebMvcConfigurer {
     }
 
     @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/favicon.ico",
+                                    "logo192.png",
+                                    "logo512.png",
+                                    "/manifest.json",
+                                    "/robots.txt")
+                .addResourceLocations("classpath:/static/");
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("classpath:/static/static/");
+    }
+
+    @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         for (String url : FRONTEND_URLS) {
-            registry.addViewController(url).setViewName("forward:/index.html");
+            registry.addViewController(url).setViewName("index.html");
         }
     }
 
