1711bf55790f6695352b5573d4066036a308b747
Nazar Horshevikov <rangar.danarim@gmail.com>
2025-06-18 00:12:37 +0300

    RELEASE fix some style bugs and favicon mapping

--- src/main/java/com/danarim/monal/config/WebConfig.java
+++ src/main/java/com/danarim/monal/config/WebConfig.java
@@ -80,9 +80,26 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/index.html",
+                                    "/apple-touch-icon-57x57.png",
+                                    "/apple-touch-icon-114x114.png",
+                                    "/apple-touch-icon-72x72.png",
+                                    "/apple-touch-icon-144x144.png",
+                                    "/apple-touch-icon-60x60.png",
+                                    "/apple-touch-icon-120x120.png",
+                                    "/apple-touch-icon-76x76.png",
+                                    "/apple-touch-icon-152x152.png",
+                                    "/favicon-196x196.png",
+                                    "/favicon-16x16.png",
+                                    "/favicon-128.png",
+                                    "/mstile-144x144.png",
+                                    "/mstile-70x70.png",
+                                    "/mstile-150x150.png",
+                                    "/mstile-310x150.png",
+                                    "/mstile-310x310.png",
                                     "/favicon.ico",
-                                    "/logo192.png",
-                                    "/logo512.png",
+
+//                                    "/logo192.png",
+//                                    "/logo512.png",
                                     "/manifest.json",
                                     "/robots.txt")
                 .addResourceLocations("classpath:/static/");
